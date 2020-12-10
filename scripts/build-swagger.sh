1e0be5fd7e0ffbb7d89ef00e09c58fb718f6975e
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-12-10 18:53:52 +0200

    updated swagger docs building script

--- scripts/build-swagger.sh
+++ scripts/build-swagger.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 API_DOC_FOLDER="./public"
 VERSIONS=( "v1" )
@@ -7,18 +7,19 @@ VERSIONS=( "v1" )
 for version in "${VERSIONS[@]}"
 do
   echo "Building schema for API $version"
-  rm -rf $API_DOC_FOLDER/$version
   mkdir -p $API_DOC_FOLDER/$version
   cp -r ./swagger/src/* $API_DOC_FOLDER/$version
+  echo "Swagger UI index file copied to the $API_DOC_FOLDER/$version"
   cp -r ./swagger/api/$version/* $API_DOC_FOLDER/$version
-  API_DOC_URL=/api/doc/$version
-  API_VERSION_URL=/api/$version
-  API_DOC_URL_ESC=$(printf '%s\n' "$API_DOC_URL" | sed -e 's/[]\/$*.^[]/\\&/g')
-  API_VERSION_URL_ESC=$(printf '%s\n' "$API_VERSION_URL" | sed -e 's/[]\/$*.^[]/\\&/g')
+  echo "Swagger schema files are copied to the $API_DOC_FOLDER/$version"
+  API_DOC_URL_ESC=$(printf '%s\n' "/api/doc/$version" | sed -e 's/[]\/$*.^[]/\\&/g')
+  API_VERSION_URL_ESC=$(printf '%s\n' "/api/$version" | sed -e 's/[]\/$*.^[]/\\&/g')
   sed -i "s/API_HOST_URL/$API_DOC_URL_ESC/g" $API_DOC_FOLDER/$version/index.html
   sed -i "s/API_VERSION_URL/$API_VERSION_URL_ESC/g" $API_DOC_FOLDER/$version/swagger.yaml
+  echo "Swagger UI index file for $version is updated to use a proper sources";
   npx js-yaml $API_DOC_FOLDER/$version/swagger.yaml > $API_DOC_FOLDER/$version/swagger.json
+  echo "Swagge yaml schema for $version is converted to the json format"
 done
 
-echo "Swagger schemas are built!"
+echo "Swagger schemas are built and Swagger UI is ready!"
 exit 0
