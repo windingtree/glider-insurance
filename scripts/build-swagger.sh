d02040118c9bf1996335c9675dc5f751ba1a71dd
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-12-10 23:54:43 +0200

    ebs9

--- scripts/build-swagger.sh
+++ scripts/build-swagger.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
-API_DOC_FOLDER="./public"
+API_DOC_FOLDER="./public/api"
 VERSIONS=( "v1" )
 
+rm -rf API_DOC_FOLDER
 mkdir -p $API_DOC_FOLDER
 
 # Build docs for all API versions
 for version in "${VERSIONS[@]}"
 do
   echo "Building schema for API $version"
-  mkdir -p $API_DOC_FOLDER/$version
-  cp -r ./swagger/src/* "$API_DOC_FOLDER/$version"
-  cp -r ./swagger/api/$version/* "$API_DOC_FOLDER/$version"
+  mkdir -p $API_DOC_FOLDER/doc/$version
+  cp -r ./swagger/src/* "$API_DOC_FOLDER/doc/$version"
+  cp -r ./swagger/api/$version/* "$API_DOC_FOLDER/doc/$version"
+
   API_DOC_URL_ESC=$(printf '%s\n' "/api/doc/$version" | sed -e 's/[]\/$*.^[]/\\&/g')
   API_VERSION_URL_ESC=$(printf '%s\n' "/api/$version" | sed -e 's/[]\/$*.^[]/\\&/g')
-  sed -i "s/API_HOST_URL/$API_DOC_URL_ESC/g" $API_DOC_FOLDER/$version/index.html
-  sed -i "s/API_VERSION_URL/$API_VERSION_URL_ESC/g" $API_DOC_FOLDER/$version/swagger.yaml
-  npx js-yaml $API_DOC_FOLDER/$version/swagger.yaml > $API_DOC_FOLDER/$version/swagger.json
-  rm -rf $API_DOC_FOLDER/$version/swagger.yaml
+  sed -i "s/API_HOST_URL/$API_DOC_URL_ESC/g" $API_DOC_FOLDER/doc/$version/index.html
+  sed -i "s/API_VERSION_URL/$API_VERSION_URL_ESC/g" $API_DOC_FOLDER/doc/$version/swagger.yaml
+
+  npx js-yaml $API_DOC_FOLDER/doc/$version/swagger.yaml > $API_DOC_FOLDER/doc/$version/swagger.json
+  rm -rf $API_DOC_FOLDER/doc/$version/swagger.yaml
 done
 
 echo "Swagger schemas are built and Swagger UI is ready!"
